43623b46ece1b111d6a2e8ca54c95c1adc32fe8f
Loki Jiang <summer10920@gmail.com>
2023-03-09 11:21:06 +0800

    service of get done

--- lokiHttp/src/app/app.component.ts
+++ lokiHttp/src/app/app.component.ts
@@ -41,21 +41,26 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.loading = true;//※重點
-    this.http.get<{ [key: string]: PostModel }>(
-      'https://loki-angular-training-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
-    ).pipe(
-      map(responseData => {
-        console.log(responseData);
-        const postAry: PostModel[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key))
-            postAry.push({ ...responseData[key], id: key })
-        }
-        return postAry;
-      })
-    ).subscribe(response => {
+
+    this.HttpService.fetchPost().subscribe(response => {
       this.loading = false; //※重點
       this.loadedPosts = response;
     });
+    // this.http.get<{ [key: string]: PostModel }>(
+    //   'https://loki-angular-training-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
+    // ).pipe(
+    //   map(responseData => {
+    //     console.log(responseData);
+    //     const postAry: PostModel[] = [];
+    //     for (const key in responseData) {
+    //       if (responseData.hasOwnProperty(key))
+    //         postAry.push({ ...responseData[key], id: key })
+    //     }
+    //     return postAry;
+    //   })
+    // ).subscribe(response => {
+    //   this.loading = false; //※重點
+    //   this.loadedPosts = response;
+    // });
   }
 }
--- lokiHttp/src/app/http.service.ts
+++ lokiHttp/src/app/http.service.ts
@@ -1,6 +1,7 @@
 import { PostModel } from './post.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,25 @@ export class HttpService {
       'https://loki-angular-training-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
       postData
     ).subscribe(response => console.log(response));
+  }
+
+  fetchPost() {
+    return this.http.get<{ [key: string]: PostModel }>(
+      'https://loki-angular-training-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
+    ).pipe(
+      map(responseData => {
+        console.log(responseData);
+        const postAry: PostModel[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key))
+            postAry.push({ ...responseData[key], id: key })
+        }
+        return postAry;
+      })
+    );
+    // ).subscribe(response => {
+    //   this.loading = false; //※重點
+    //   this.loadedPosts = response;
+    // });
   }
 }
