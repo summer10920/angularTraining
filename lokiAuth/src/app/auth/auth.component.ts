9bbf62fe39b4417dfc6becdbe848bf9b8dae9fcc
Loki Jiang <summer10920@gmail.com>
2023-03-10 13:39:31 +0800

    signup submit to api

--- lokiAuth/src/app/auth/auth.component.ts
+++ lokiAuth/src/app/auth/auth.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth.service';
 import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
 
@@ -8,11 +9,28 @@ import { Component } from '@angular/core';
 export class AuthComponent {
   isLoginMode = true;
 
+  constructor(
+    private AuthService: AuthService
+  ) { }
+
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    // console.log(form.value);
+    if (form.invalid) return;
+
+    if (this.isLoginMode) {
+      // 登入作業
+    } else {
+      // 註冊作業
+      this.AuthService.singUp(form.value.email, form.value.password).subscribe(
+        resData => console.log(resData),
+        error => console.error(error)
+      );
+    }
+
+    form.reset();
   }
 }
