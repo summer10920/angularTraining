7e720a668a38bb49d09e790ce285e99645880246
Loki Jiang <summer10920@gmail.com>
2023-03-13 10:42:16 +0800

    error alert of signIn

--- lokiAuth/src/app/auth/auth.service.ts
+++ lokiAuth/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -9,7 +9,7 @@ export interface AuthResponseData {
   refreshToken: string; // Firebase 身份驗證刷新token
   expiresIn: string; // token過期的秒數
   localId: string; // 用戶的uid
-  registered?:boolean; // 電子郵件是否用於現有帳戶
+  registered?: boolean; // 電子郵件是否用於現有帳戶
 }
 
 @Injectable({
@@ -31,24 +31,24 @@ export class AuthService {
         returnSecureToken: true
       }
     ).pipe(
-      catchError(err => {
-        let errorMsg = 'unknown Error';
-        if (!err.error || !err.error.error) return throwError(errorMsg);
+      catchError(this.errorHandle)
+      // catchError(err => {
+      //   let errorMsg = 'unknown Error';
+      //   if (!err.error || !err.error.error) return throwError(errorMsg);
 
-        switch (err.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMsg = '電子郵件地址已被另一個帳戶使用。';
-            break;
-          case 'OPERATION_NOT_ALLOWED':
-            errorMsg = '此項目禁用密碼登錄。';
-            break;
-          case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-            errorMsg = '由於異常活動，我們已阻止來自此設備的所有請求。稍後再試。';
-            break;
-        }
-
-        return throwError(errorMsg);
-      })
+      //   switch (err.error.error.message) {
+      //     case 'EMAIL_EXISTS':
+      //       errorMsg = '電子郵件地址已被另一個帳戶使用。';
+      //       break;
+      //     case 'OPERATION_NOT_ALLOWED':
+      //       errorMsg = '此項目禁用密碼登錄。';
+      //       break;
+      //     case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+      //       errorMsg = '由於異常活動，我們已阻止來自此設備的所有請求。稍後再試。';
+      //       break;
+      //   }
+      //   return throwError(errorMsg);
+      // })
     );
   }
 
@@ -60,6 +60,36 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
+    ).pipe(
+      catchError(this.errorHandle)
     );
+  }
+
+  private errorHandle(errorRes: HttpErrorResponse) {
+    let errorMsg = 'unknown Error';
+    if (!errorRes.error || !errorRes.error.error) return throwError(errorMsg);
+
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMsg = '電子郵件地址已被另一個帳戶使用。';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMsg = '此項目禁用密碼登錄。';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMsg = '由於異常活動，我們已阻止來自此設備的所有請求。稍後再試。';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMsg = '沒有與此標識符對應的用戶記錄。該用戶可能已被刪除。';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMsg = '密碼無效或用戶沒有密碼。';
+        break;
+      case 'USER_DISABLED':
+        errorMsg = '用戶帳戶已被管理員禁用。';
+        break;
+    }
+
+    return throwError(errorMsg);
   }
 }
