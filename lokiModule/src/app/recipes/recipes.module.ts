54e70c9aabb0ccb863c61aee7a7086f33f0c978a
Loki Jiang <summer10920@gmail.com>
2023-03-15 16:55:10 +0800

    拆分 appModule

---
+++ lokiModule/src/app/recipes/recipes.module.ts
@@ -0,0 +1,36 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
+  imports: [
+    CommonModule, //for ngif
+    RouterModule, // for route path
+    ReactiveFormsModule, // for formGroup
+  ],
+  exports: [
+    // RecipesComponent,
+    // RecipeListComponent,
+    // RecipeDetailComponent,
+    // RecipeItemComponent,
+    // RecipeStartComponent,
+    // RecipeEditComponent,
+  ],
+})
+export class RecipesModule { }
