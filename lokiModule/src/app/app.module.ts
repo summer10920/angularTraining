599f77d0ae9d20cb3ba35be63e7d712171ffb53f
Loki Jiang <summer10920@gmail.com>
2023-03-16 14:09:50 +0800

    coreModule

--- lokiModule/src/app/app.module.ts
+++ lokiModule/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -6,16 +5,15 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-// import { DropdownDirective } from './shared/dropdown.directive';
-// import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-// import { AlertComponent } from './shared/alert/alert.component';
 import { SharedModule } from './shared/shared.module';
+// import { AuthInterceptorService } from './auth/auth-interceptor.service';
+// import { ShoppingListService } from './shopping-list/shopping-list.service';
+// import { RecipeService } from './recipes/recipe.service';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -34,16 +32,17 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService, {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    }
-  ],
+  // providers: [
+  //   ShoppingListService,
+  //   RecipeService, {
+  //     provide: HTTP_INTERCEPTORS,
+  //     useClass: AuthInterceptorService,
+  //     multi: true
+  //   }
+  // ],
   bootstrap: [AppComponent],
   // entryComponents: [
   //   AlertComponent,
